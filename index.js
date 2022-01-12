621ffac587179f054a33465e17fd532327bbbeae
phurinutw <phurinut.wara@gmail.com>
2022-01-12 15:22:59 +0700

    Finish index.js to generate text file for use in bulk insert for sequelize seeding

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
-const path = require('path');
-const csv = require('fast-csv');
+const csv = require('csv-parser');
+
+let results = []
+
+fs.createReadStream('./airport.csv')
+    .pipe(csv({ headers: false }))
+    .on('data', (row) => {
+        // results.push(row)
+        results.push(`{code:\`${row[0]}\`,name:\`${row[1]}\`,country_code:\`${row[2]}\`,timezone:\`${row[3]}\`,created_at: new Date(),},`)
+    })
+    .on('end', () => {
+        console.log(results) // preview results to log
+        results.shift() // To delete header row from results
+        fs.writeFileSync('./seed.txt', results.join('\n'))
+    })
